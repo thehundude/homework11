510b7e957e239a0aa8b0e93eb1c405b663919163
thehundude <tamas.reisz@outlook.com>
2017-05-28 13:13:50 +0200

    #4 convertYoutubeJSONToXML
    Nem tudtam rájönni, hogy tudtam volna használi a fasterxml-t az xml készítéshez, ezért stack overflow-n néztem körbe.
    
    https://stackoverflow.com/questions/19977979/converting-json-to-xml-in-java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Util.printYoutubeJSON("C:\\Users\\tamas\\IdeaProjects\\homework11\\src\\main\\java\\json\\youtube.json");
+        // Util.printYoutubeJSON("C:\\Users\\tamas\\IdeaProjects\\homework11\\src\\main\\java\\json\\youtube.json");
+        Util.convertYoutubeJSONToXML("C:\\Users\\tamas\\IdeaProjects\\homework11\\src\\main\\java\\json\\youtube.json",
+                "C:\\Users\\tamas\\IdeaProjects\\homework11\\homework11.xml");
     }
 }
--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -1,6 +1,10 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
 import json.Youtube;
+import org.json.JSONObject;
+import org.json.XML;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -12,5 +16,20 @@ public class Util {
 
         Youtube youtube = objectMapper.readValue(jsonData, Youtube.class);
         System.out.println(objectMapper.writeValueAsString(youtube));
+    }
+
+    public static void convertYoutubeJSONToXML(String path, String target) throws IOException {
+        byte [] jsonData = Files.readAllBytes(Paths.get(path));
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        Youtube youtube = objectMapper.readValue(jsonData, Youtube.class);
+
+        JSONObject json = new JSONObject(youtube);
+        String xml = XML.toString(json);
+
+        FileWriter file = new FileWriter(target);
+        BufferedWriter buffer = new BufferedWriter(file);
+        buffer.write(xml);
+        buffer.close();
     }
 }
